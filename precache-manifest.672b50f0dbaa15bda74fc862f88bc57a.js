2b3f64ed5d92923fbf5055d27f9e73ed7cfb8449
IvanVilchinskij <67802011+IvanVilchinskij@users.noreply.github.com>
2021-08-27 17:29:46 +0300

    Add files via upload

---
+++ precache-manifest.672b50f0dbaa15bda74fc862f88bc57a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86dfc5c17bcf25396b1",
+    "url": "/duet/css/Accelerometer.87b44128.css"
+  },
+  {
+    "revision": "05e043e38c99373a4c37",
+    "url": "/duet/css/GCodeViewer.7e64d9ff.css"
+  },
+  {
+    "revision": "839f9d41d1568112691f",
+    "url": "/duet/css/HeightMap.cf36616a.css"
+  },
+  {
+    "revision": "8724c15791a0749cc509",
+    "url": "/duet/css/ObjectModelBrowser.17a75c39.css"
+  },
+  {
+    "revision": "5e8ffd87ccf784a3b4ad",
+    "url": "/duet/css/OnScreenKeyboard.767659a0.css"
+  },
+  {
+    "revision": "9d96a9a1fa20155917ee",
+    "url": "/duet/css/app.f0795770.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/duet/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/duet/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/duet/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/duet/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "e5958695a72278cf6603036f832c7e5b",
+    "url": "/duet/index.html"
+  },
+  {
+    "revision": "c86dfc5c17bcf25396b1",
+    "url": "/duet/js/Accelerometer.d161d34d.js"
+  },
+  {
+    "revision": "05e043e38c99373a4c37",
+    "url": "/duet/js/GCodeViewer.bb2a6990.js"
+  },
+  {
+    "revision": "839f9d41d1568112691f",
+    "url": "/duet/js/HeightMap.d69ea399.js"
+  },
+  {
+    "revision": "8724c15791a0749cc509",
+    "url": "/duet/js/ObjectModelBrowser.1c0c1c36.js"
+  },
+  {
+    "revision": "5e8ffd87ccf784a3b4ad",
+    "url": "/duet/js/OnScreenKeyboard.43a096a7.js"
+  },
+  {
+    "revision": "9d96a9a1fa20155917ee",
+    "url": "/duet/js/app.5c481f9c.js"
+  },
+  {
+    "revision": "f5a3f67027690d7c10ad38afee1941f1",
+    "url": "/duet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/duet/robots.txt"
+  }
+]);
